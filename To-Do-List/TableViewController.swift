693e6478f319aeec58b893032f987d562cc499c5
Abilat01 <Abilat01@yandex.ru>
2021-09-03 21:45:28 +0300

    CoreData save and deleteAll

--- To-Do-List/TableViewController.swift
+++ To-Do-List/TableViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
     
     //массив с задачами
-    var tasks: [String] = []
+    var tasks: [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch let erron as NSError {
+            print(erron.localizedDescription)
+        }
     }
     
     
@@ -22,8 +38,8 @@ class TableViewController: UITableViewController {
         let alertControler = UIAlertController(title: "Новая задача", message: "Запишите новую задачу", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Сохранить", style: .default) { action in
             let tf = alertControler.textFields?.first
-            if let saveTask = tf?.text {
-                self.tasks.append(saveTask)
+            if let saveTaskTF = tf?.text {
+                self.saveTask(withTitle: saveTaskTF)
                 self.tableView.reloadData()
             }
         }
@@ -36,10 +52,50 @@ class TableViewController: UITableViewController {
         present(alertControler, animated: true, completion: nil)
     }
     
+    //
+    private func saveTask(withTitle title: String) {
+        
+        let context = getContext()
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    //
+    private func getContext() -> NSManagedObjectContext {
+        
+        let AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        return AppDelegate.persistentContainer.viewContext
+        
+    }
+    
+    //
     @IBAction func deleteAll(_ sender: UIBarButtonItem) {
         
-        tasks.removeAll()
-        tableView.reloadData()
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        if let objects = try? context.fetch(fetchRequest) {
+            for object in objects {
+                context.delete(object)
+            }
+        }
+        
+        do {
+            try context.save()
+            tableView.reloadData()
+        } catch let error {
+            print(error.localizedDescription)
+        }
         
     }
     
@@ -60,7 +116,8 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        cell.textLabel?.text = tasks[indexPath.row]
+        let tasksText = tasks[indexPath.row]
+        cell.textLabel?.text = tasksText.title
 
         return cell
     }
