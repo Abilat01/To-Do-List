b6a037e0cd9069303d59976322360e8f805ade2f
Abilat01 <Abilat01@yandex.ru>
2021-09-03 18:44:36 +0300

    Initial Commit

---
+++ To-Do-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do-List
+//
+//  Created by Danya on 03.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
